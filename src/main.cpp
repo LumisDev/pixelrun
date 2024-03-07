5914b115dd30a2b4a6cee413932a978339d17238
LumisDev <153009693+LumisDev@users.noreply.github.com>
2024-03-07 17:16:53 +0100

    Reset GLAD

--- src/main.cpp
+++ src/main.cpp
@@ -131,7 +131,7 @@ int main(int argc, char* argv[]) {
     setupOpenGL();
 
     // Main loop
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     VBInt vb = initVBData();
 
     bool quit = false;
--- src/shaders/default.frag
+++ src/shaders/default.frag
@@ -1,6 +1,6 @@
 #version 410 core
 layout (location=0) out vec4 fragColor;
-
+#extension GL_ARB_shading_language_include: require
 void main() {
     fragColor = vec4(0.5, 0.3, 0.5, 1.0);
 }
